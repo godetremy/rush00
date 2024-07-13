7f5ccab50f9f205f7879c9c66225458450e513e9
Remy Godet <rgodet@3C5.42angouleme.fr>
2024-07-13 10:37:06 +0200

    ex00 : Added rush func

--- ex00/rush02.c
+++ ex00/rush02.c
@@ -6,15 +6,51 @@
 /*   By: rgodet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 08:45:01 by rgodet            #+#    #+#             */
-/*   Updated: 2024/07/13 09:01:05 by rgodet           ###   ########.fr       */
+/*   Updated: 2024/07/13 10:34:22 by rgodet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_putchar.c"
-#include <unistd.h>
+
+void print_line_middle(int x)
+{
+	x--;
+	ft_putchar('B');
+	while(x > 1)
+	{
+		ft_putchar(' ');
+		x--;
+	}
+	if (x > 0)
+		ft_putchar('B');
+	ft_putchar(10);
+}
+
+void print_line_ac(int x, char c)
+{
+	x--;
+	ft_putchar(c);
+	while(x > 1)
+	{
+		ft_putchar('B');
+		x--;
+	}
+	if (x > 0)
+		ft_putchar(c);
+	ft_putchar(10);
+}
 
 void rush(int x, int y)
 {
-	ft_putchar('A');
-	ft_putchar(10);
+	if (x <= 0 || y <= 0)
+		return ;
+	y--;
+	print_line_ac(x, 'A');
+	while(y > 1)
+	{
+		print_line_middle(x);
+		y--;
+	}
+	if (y > 0)
+		print_line_ac(x, 'C');
 }
